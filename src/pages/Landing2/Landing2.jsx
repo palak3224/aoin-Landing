d4fc213ecfc53ec5676285fbc43e720142941cf7
Tech-Avrach <nikhilpatel9226@gmail.com>
2025-09-10 14:22:07 +0530

    Final Cta done

--- src/pages/Landing2/Landing2.jsx
+++ src/pages/Landing2/Landing2.jsx
@@ -8,6 +8,7 @@ import StorePreviewSection from './components/StorePreviewSection'
 import FreeFeatures from './components/FreeFeatures'
 import AoinBusinessPackage from './components/AoinBusinessPackage'
 import TestimonialCarousel from './components/TestimonialCarousel'
+import BusinessStartSection from './components/BusinessStartSection'
 
 const Landing2 = () => {
   return (
@@ -21,6 +22,7 @@ const Landing2 = () => {
       <FreeFeatures />
       <AoinBusinessPackage />
       <TestimonialCarousel />
+      <BusinessStartSection />
     </div>
   )
 }
